0d649e752a9450d88f68458760898cfdb74dcf6e
KhoaPhan1504 <Khoahttt01@gmail.com>
2024-07-09 11:28:10 +0700

    Design DB, connect to MongoDB Successfull

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3333;
 
 app.get('/', (req, res) => {
     return res.send('Hello World');
 })
+
+mongoose.connect(`mongodb+srv://khoapaforworkit:${process.env.MONGO_DB}@cluster0.hbhugof.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+    .then(() => {
+    console.log('Connect DB successfull');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
 app.listen(port, () => {
     console.log(`App listening on port http://localhost:${port}`);
